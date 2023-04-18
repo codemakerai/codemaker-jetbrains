47ae38fc3fa0689d0d7e932ea98b7fe505384cff
codemakerai-dev <codemaker-dev@codemaker.ai>
2023-04-17 19:21:28 -0700

    Bump the version to 1.1.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ai.codemaker.jetbrains"
-version = "1.0.1-SNAPSHOT"
+version = "1.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/java/ai/codemaker/sdk/client/DefaultClient.java
+++ src/main/java/ai/codemaker/sdk/client/DefaultClient.java
@@ -52,7 +52,7 @@ public class DefaultClient implements Client {
             final byte[] body = serialize(request);
             final ClassicHttpRequest httpRequest = ClassicRequestBuilder.post(endpoint(path))
                     .addHeader("Accept", "application/json")
-                    .addHeader("User-Agent", "CodeMakerSdkJava/0.0.1")
+                    .addHeader("User-Agent", "CodeMakerSdkJava/1.1.0")
                     .addHeader("Authorization", String.format("Bearer %s", apiKey))
                     .setEntity(body, ContentType.APPLICATION_JSON)
                     .build();
