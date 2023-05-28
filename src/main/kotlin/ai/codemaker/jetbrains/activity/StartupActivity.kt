490dd07fd3f0d81c0e8643725f1af33f49b37b59
codemakerai-dev <codemaker-dev@codemaker.ai>
2023-05-28 16:55:23 -0700

    Displaying notification during startup

---
+++ src/main/kotlin/ai/codemaker/jetbrains/activity/StartupActivity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 CodeMaker AI Inc. All rights reserved.
+ */
+
+package ai.codemaker.jetbrains.activity
+
+import ai.codemaker.jetbrains.image.Icons
+import ai.codemaker.jetbrains.settings.AppSettingsConfigurable
+import ai.codemaker.jetbrains.settings.AppSettingsState
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import javax.swing.ImageIcon
+
+class StartupActivity : ProjectActivity {
+    companion object {
+        private const val NOTIFICATION_TITLE = "CodeMaker AI"
+        private const val NOTIFICATION_MESSAGE = "Configure JetBrains plugin."
+        private const val NOTIFICATION_ICON = "icons/icon-small.svg"
+    }
+
+    override suspend fun execute(project: Project) {
+        val settings = AppSettingsState.instance
+        if (settings.apiKey.isNullOrBlank()) {
+            val notificationGroup = NotificationGroupManager.getInstance()
+                    .getNotificationGroup("CodeMaker AI")
+            val notification = notificationGroup.createNotification(
+                    NOTIFICATION_TITLE, NOTIFICATION_MESSAGE, NotificationType.INFORMATION)
+            notification.icon = Icons.Logo
+            notification.addAction(object : AnAction("Configure") {
+                override fun actionPerformed(e: AnActionEvent) {
+                    ShowSettingsUtil.getInstance().showSettingsDialog(project, AppSettingsConfigurable::class.java);
+                }
+            })
+            notification.notify(project)
+        }
+    }
+}
---
+++ src/main/kotlin/ai/codemaker/jetbrains/image/Icons.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2023 CodeMaker AI Inc. All rights reserved.
+ */
+
+package ai.codemaker.jetbrains.image
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val Logo = IconLoader.getIcon("/icons/icon-small.svg", javaClass)
+}
