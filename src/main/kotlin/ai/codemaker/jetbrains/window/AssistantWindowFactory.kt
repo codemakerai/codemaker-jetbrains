80452819985c0f4f849a8a5e7cc860c59cbf0ea8
codemakerai-dev <codemaker-dev@codemaker.ai>
2024-01-22 18:59:06 -0800

    Implemented appending of the messages to the view

--- src/main/kotlin/ai/codemaker/jetbrains/window/AssistantWindowFactory.kt
+++ src/main/kotlin/ai/codemaker/jetbrains/window/AssistantWindowFactory.kt
@@ -7,19 +7,21 @@ package ai.codemaker.jetbrains.window
 import ai.codemaker.jetbrains.assistant.Message
 import ai.codemaker.jetbrains.assistant.Role
 import ai.codemaker.jetbrains.service.CodeMakerService
-import com.intellij.ide.DataManager
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.jcef.JBCefApp
+import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.util.ui.JBUI
+import org.cef.browser.CefBrowser
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
 import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.event.ActionEvent
@@ -27,13 +29,8 @@ import java.awt.event.ActionListener
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 import java.util.*
-import java.util.concurrent.TimeUnit
-import javax.swing.BorderFactory
 import javax.swing.JButton
-import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.JScrollPane
-import javax.swing.JTextArea
 import javax.swing.JTextField
 
 
@@ -47,7 +44,7 @@ class AssistantWindowFactory : ToolWindowFactory, DumbAware {
     class AssistantWindow(val project: Project) {
 
         val contentPanel = JPanel()
-        val chatArea = JTextArea()
+        val chatScreen = JBCefBrowser()
         val messageTextField = JTextField()
 
         val messages = ArrayList<Message>()
@@ -60,17 +57,19 @@ class AssistantWindowFactory : ToolWindowFactory, DumbAware {
         }
 
         private fun createChatPanel(): Component {
-            chatArea.isEditable = false
-            chatArea.lineWrap = true
-            chatArea.wrapStyleWord = true
-            return JBScrollPane(chatArea)
+            if (!JBCefApp.isSupported()) {
+                // TODO log warning
+            }
+
+            chatScreen.loadHTML(chatHtml())
+            return chatScreen.component
         }
 
         private fun createMessagePanel(): Component {
             val messagePanel = JPanel()
             messagePanel.layout = BorderLayout()
             messagePanel.border = JBUI.Borders.empty(10)
-            messageTextField.addKeyListener( MessageTextKeyListener())
+            messageTextField.addKeyListener(MessageTextKeyListener())
             messagePanel.add(messageTextField, BorderLayout.CENTER)
 
             val sendButton = JButton("Send")
@@ -81,13 +80,11 @@ class AssistantWindowFactory : ToolWindowFactory, DumbAware {
         }
 
         private fun addMessage(input: String, role: Role) {
+            // TODO cap message size
             messages.add(Message(UUID.randomUUID().toString(), input, role, Date()))
 
-            if (chatArea.text.isNotEmpty()) {
-                chatArea.insert("\n", chatArea.text.length)
-            }
-
-            chatArea.insert(input, chatArea.text.length)
+            val content = renderMarkdown(input).replace("\"", "\\\"")
+            chatScreen.cefBrowser.executeJavaScript("window.append(\"$content\")", "", 0)
         }
 
         private fun sendMessage() {
@@ -112,6 +109,32 @@ class AssistantWindowFactory : ToolWindowFactory, DumbAware {
                     addMessage(output, Role.Assistant)
                 }
             }
+        }
+
+        private fun chatHtml(): String {
+            // language=HTML
+            return """
+                <div id="container" style="width:100%; height:100%;">
+                  <div id="chat">
+                  </div>
+                  <span id="anchor"></span>
+                </div>
+                <script>
+                    window.append = function(body) {
+                        let message = document.createElement("div");                                                
+                        message.style.cssText = "background-color:#f1f1f1;padding:10px;border:2px solid #dedede;border-radius:5px;margin:10px 0;";
+                        message.innerHTML = body;
+                        document.getElementById("chat").appendChild(message);
+                        document.getElementById("anchor").scrollIntoView({ behavior: "smooth"});
+                    }
+                </script>
+                """.trimIndent()
+        }
+
+        private fun renderMarkdown(source: String): String {
+            val flavour = CommonMarkFlavourDescriptor()
+            val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(source)
+            return HtmlGenerator(source, parsedTree, flavour).generateHtml()
         }
 
         inner class SendActionListener : ActionListener {
